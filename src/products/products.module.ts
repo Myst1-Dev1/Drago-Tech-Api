5e2067fc2f923a2ec9df850599aedd470d8aaf73
João Victor <joao.victor@internit.net.br>
2025-10-21 22:12:55 -0300

    testing new secret key

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,6 +4,8 @@ import { ProductsService } from './products.service';
 import { FileUploadService } from 'src/file-upload/file-upload.service';
 import { PrismaService } from '../prisma/prisma.service';
 
+//teste
+
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService, FileUploadService, PrismaService],
