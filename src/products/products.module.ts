b6a9b2a4a74980805ef30026a644159999813197
João Victor <joao.victor@internit.net.br>
2025-10-22 00:46:13 -0300

    testing ci cd again

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,7 +4,7 @@ import { ProductsService } from './products.service';
 import { FileUploadService } from 'src/file-upload/file-upload.service';
 import { PrismaService } from '../prisma/prisma.service';
 
-//teste 2
+//teste 3
 
 @Module({
   controllers: [ProductsController],
