840334b337735e6f8bb03d19c5b4b8cf378bc0f8
João Victor <joao.victor@internit.net.br>
2025-10-24 00:17:25 -0300

    adjust on register dto

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -9,21 +9,27 @@ export class RegisterDto {
   @MinLength(6, { message: 'A senha deve conter 6 ou mais caracteres' })
   password: string;
 
+  @IsString()
   @IsNotEmpty()
   name: string;
 
+  @IsString()
   @IsNotEmpty()
   address: string;
 
+  @IsString()
   @IsNotEmpty()
   zipCode: string;
 
+  @IsString()
   @IsNotEmpty()
   phone: string;
 
+  @IsString()
   @IsNotEmpty()
   city: string;
 
+  @IsString()
   @IsNotEmpty()
   state: string;
 }
